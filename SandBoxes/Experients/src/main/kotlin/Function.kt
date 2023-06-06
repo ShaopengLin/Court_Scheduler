5b4a96546651ed132fc6fb2792ac77b164636819
Shaopeng (Chris) Lin <shaopeng.lin@mail.utoronto.ca>
2023-06-06 19:45:40 -0400

    Functions code added

--- SandBoxes/Experients/src/main/kotlin/Function.kt
+++ SandBoxes/Experients/src/main/kotlin/Function.kt
@@ -1,4 +1,24 @@
-fun facadeFunction()
-{
+import kotlin.random.Random
 
+fun facadeFunction(args: Array<String>) {
+    println("Good ${if (args[0].toInt() < 12) "morning" else "night"}, Kotlin")
+    println(randomDay())
+}
+
+fun randomDay(): String {
+    val weekday = arrayOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+    return weekday[Random.nextInt(7)]
+}
+
+fun getFortuneCookie(): String {
+    val fortuneList: Array<String> = arrayOf(
+        "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!"
+    )
+    var input : UInt = (readlnOrNull()?.toUIntOrNull() ?: 1) as UInt
+    return fortuneList[input.mod(fortuneList.size.toUInt()).toInt()]
 }
